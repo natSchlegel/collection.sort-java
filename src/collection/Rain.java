19ca6ec95ca7acbb4ac9f2807eaf355065087161
natSchlegel <nataschaschlegel27@gmail.com>
2022-04-30 23:16:01 -0300

    Sysout Updated
    Update if to show only month's median above the semester's median.

--- src/collection/Rain.java
+++ src/collection/Rain.java
@@ -9,6 +9,7 @@ public class Rain {
 
 	String month;
 	int median;
+
 	
 	
 	public Rain(String string, int i) {
@@ -18,7 +19,7 @@ public class Rain {
 
 	public static void main(String[] args) {
 		
-		
+		int soma = 0;
 		ArrayList<Rain> rain = new ArrayList<>();
 		for (int i = 0; i < 6; i++) {
 			Scanner scan = new Scanner(System.in);
@@ -29,17 +30,24 @@ public class Rain {
 			rain.add(new Rain(month,median));
 		}
 		
+		for(int i = 0; i < rain.size(); i++) {
+
+			soma = soma + rain.get(i).median;
+		}
+		
+		soma = soma / rain.size();
+		
 		Collections.sort(rain, new Comparator<Rain>() {
 			public int compare(Rain r1, Rain r2) {
 				return Integer.valueOf(r1.median).compareTo(r2.median);
 			}
 		});
 		
-		System.out.println("Média de Chuvas: (ordem crescente)");
 		for (int i = 0; i < rain.size(); i++) {
+			if(rain.get(i).median > soma) {
 			System.out.println(i + " - " + rain.get(i).month);
 			
 		}		
 	}
-
+	}
 }
